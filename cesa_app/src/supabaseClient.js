a0d5b201754dbd71b9a697062d3ba77dcd9098bd
Alan Lares <emiliano4201877@gmail.com>
2025-11-04 10:16:16 -0600

    Actualizacion supabase

--- cesa_app/src/supabaseClient.js
+++ cesa_app/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://lrkpmnausxkvcuwmptqm.supabase.co"; // URL
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxya3BtbmF1c3hrdmN1d21wdHFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMTU2NjMsImV4cCI6MjA3Njg5MTY2M30.bSGx3cRDmRyp0WlYPC6yNuNAujC0F1GZXtVu76kb4rA"; // clave pública
+const supabaseUrl = "https://fysfjscjaewotikomjjm.supabase.co"; // URL
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5c2Zqc2NqYWV3b3Rpa29tamptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzOTEzNDMsImV4cCI6MjA3NTk2NzM0M30.CJFAm93WZBGKfGLrNX1BSwQpn7DDzIMfstDHy59wuUY"; // clave pública
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
