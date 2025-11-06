18c1a14c0061a18fb119e5449fec0cfc83abee98
santo117 <santiagoabisaigonzalezgarcia@gmail.com>
2025-11-06 06:53:13 -0600

    subbiendo la conexion

--- cesa_app/src/supabaseClient.js
+++ cesa_app/src/supabaseClient.js
@@ -5,5 +5,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://fysfjscjaewotikomjjm.supabase.co'; 
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5c2Zqc2NqYWV3b3Rpa29tamptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzOTEzNDMsImV4cCI6MjA3NTk2NzM0M30.CJFAm93WZBGKfGLrNX1BSwQpn7DDzIMfstDHy59wuUY';
 
-// 👈 Exporta la instancia de supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
